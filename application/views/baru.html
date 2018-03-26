92696c8262dd9930155a58ee090c360a0826e5b3
istnaluthfi <luthfiistna@gmail.com>
2018-03-26 15:39:17 +0700

    pemasukan data

---
+++ application/views/baru.html
@@ -0,0 +1,74 @@
+<html>
+
+<head>
+<meta http-equiv="Content-Language" content="id">
+<meta name="GENERATOR" content="Microsoft FrontPage 5.0">
+<meta name="ProgId" content="FrontPage.Editor.Document">
+<meta http-equiv="Content-Type" content="text/html; charset=windows-1252">
+<title>WOMENS</title>
+</head>
+
+<body background="../PICTURE/Untitled.jpg">
+
+<div align="center">
+  <center>
+  <table border="1" cellpadding="0" cellspacing="0" style="border-collapse: collapse; border-width: 0" bordercolor="#000000" width="846" id="AutoNumber1" height="134" bgcolor="#FFFFFF">
+    <tr>
+      <td height="129" width="241" style="border-style: none; border-width: medium">
+      <p align="center">
+      <a href="../home.htm">
+      <img border="0" src="../PICTURE/LOGO2%20copy.jpg" width="226" height="73"></a></td>
+      <td height="129" width="134" style="border-style: none; border-width: medium" align="center">
+      <a style="text-decoration: none" href="BRANDS.htm">
+      <font color="#000000">BRANDS</font></a></td>
+      <td height="129" width="126" style="border-style: none; border-width: medium" align="center">
+      <a style="text-decoration: none" href="SHOES.htm">
+      <font color="#000000">SHOES</font></a></td>
+      <td height="129" width="102" style="border-style: none; border-width: medium" align="center">
+      <a style="text-decoration: none" href="MENS.htm">
+      <font color="#000000">MENS</font></a></td>
+      <td height="129" width="149" style="border-style: none; border-width: medium" align="center">
+      <a style="text-decoration: none" href="WOMENS.htm">
+      <font color="#000000">WOMENS</font></a></td>
+      <td height="129" width="87" style="border-style: none; border-width: medium" align="center">
+      <a style="text-decoration: none" href="BOYS.htm">
+      <font color="#000000">BOYS</font></a></td>
+      <td height="129" width="178" style="border-style: none; border-width: medium" align="center">
+      <a href="ABOUT%20ME.htm" style="text-decoration: none">
+      <font color="#000000">ABOUT ME</font></a></td>
+    </tr>
+    <tr>
+      <td height="19" width="1769" style="border-style: none; border-width: medium" colspan="7">
+      <img border="0" src="../PICTURE/FREE%20.jpg" width="938" height="57"></td>
+    </tr>
+    <tr>
+      <td height="5" width="1769" style="border-style: none; border-width: medium" colspan="7">
+      <img border="0" src="../PICTURE/womens-winter-deals-748x350-5.jpg" width="937" height="350"></td>
+    </tr>
+    <tr>
+      <td height="19" width="1769" style="border-style: none; border-width: medium" colspan="7">&nbsp;
+      </td>
+    </tr>
+    <tr>
+      <td height="19" width="1769" style="border-style: none; border-width: medium" colspan="7">
+      <img border="0" src="../PICTURE/zine-mix-and-save-748x350.jpg" width="938" height="350"></td>
+    </tr>
+    <tr>
+      <td height="19" width="1769" style="border-style: none; border-width: medium" colspan="7">&nbsp;
+      </td>
+    </tr>
+    <tr>
+      <td height="19" width="1769" style="border-style: none; border-width: medium" colspan="7">
+      <img border="0" src="../PICTURE/stance-women-boobies-748x350.jpg" width="938" height="350"></td>
+    </tr>
+    <tr>
+      <td height="19" width="1769" style="border-style: none; border-width: medium" colspan="7">
+      <img border="0" src="../PICTURE/FOOTER.jpg" width="936" height="54"></td>
+    </tr>
+  </table>
+  </center>
+</div>
+
+</body>
+
+</html>
---
+++ application/views/home.php
@@ -0,0 +1,339 @@
+<!DOCTYPE html>
+<html lang="en">
+
+  <head>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Tugas Web Framework</title>
+
+    <!-- Bootstrap core CSS -->
+    <link rel="stylesheet" href="assets/vendor/bootstrap/css/bootstrap.min.css">
+
+    <!-- Custom fonts for this template -->
+    <link href="vendor/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+    <link href='https://fonts.googleapis.com/css?family=Lora:400,700,400italic,700italic' rel='stylesheet' type='text/css'>
+    <link href='https://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800' rel='stylesheet' type='text/css'>
+
+    <!-- Custom styles for this template -->
+    <link rel ="stylesheet" href="assets/css/clean-blog.min.css">
+
+  </head>
+
+  <body>
+
+    <!-- Navigation -->
+    <nav class="navbar navbar-expand-lg navbar-light fixed-top" id="mainNav">
+      <div class="container">
+        <a class="navbar-brand" href="#page-top"><img src="assets/img/logo.jpg" width="60" height="40"></a>
+        <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+          Menu
+          <i class="fa fa-bars"></i>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarResponsive">
+          <ul class="navbar-nav ml-auto">
+            <li class="nav-item">
+              <a class="nav-link js-trigger" href="#">Home</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#about">About</a>
+            </li>
+           
+            <li class="nav-item">
+              <a class="nav-link" href="#image">Image</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+
+    <!-- Page Header -->
+    <header class="masthead" style="background-image: url('assets/img/zalora.png')">
+      <div class="overlay"></div>
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-8 col-md-10 mx-auto">
+            <div class="site-heading">
+              <h1>Fashion Shop</h1>
+              <span class="subheading">Z A L O R A</span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </header>
+
+    <!-- Main Content -->
+    <tr>
+    
+   <span id="about"></span>   
+   <center><b>
+    Belanja Online di ZALORA Indonesia
+     
+   </center> </b>
+<br>
+
+ZALORA Indonesia merupakan yang terdepan dalam belanja fashion online, menyediakan brand lokal dan internasional yang terus bertambah untuk para konsumer di seluruh Indonesia. Kami memiliki lebih dari 150.000 produk yang dapat memenuhi kebutuhan fashion Anda, mulai dari rok hingga gaun, sepatu kets hingga sandal, pakaian olahraga hingga jam tangan, dan masih banyak lagi.
+
+Awali penemuan gaya Anda dengan memiliki beberapa perlengkapan fashion dasar yang penting, seperti kaos dan celana denim jeans, celana pendek dan kacamata, dan kebutuhan fashion dasar lainnya. Temukan inspirasi fashion pilihan kami, mulai dari pakaian formal hingga dress untuk ke pesta. Tampil menawan dengan mengenakan jas dan blazer untuk pergi ke kantor. Jangan lupa untuk memadukan keseluruhan penampilan Anda dengan sepatu, ada banyak sekali pilihannya mulai dari flats, high heels, boots, atau sepatu formal. Persiapkan diri Anda untuk segala musim dengan berbelanja berbagai macam outerwear, mulai dari jaket ringan hingga sweater yang nyaman. Koleksi fashion kami pun dilengkapi dengan berbagai macam aksesoris -- seperti tas dan perlengkapan olahraga. Para pelanggan ZALORA Indonesia juga dapat berbelanja berdasarkan tren terbaru yang sedang mendominasi di dunia fashion, apakah itu gaya monokrom, athleisure, atau tren terbaru di musim ini
+</br>
+
+<br>
+<center><b> Belanja Fashion di Toko Online ZALORA Indonesia!
+</center></b>
+
+
+ZALORA Indonesia adalah toko online dimana brand fashion favorit Anda berada. Dengan berbelanja fashion di toko online ZALORA Indonesia Anda dapat menemukan produk fashion eksklusif terbaik dari ribuan fashion brand terkenal, serta produk-produk yang dijamin original. Kami sebagai pusat belanja fashion online terbesar di Indonesia dapat menjawab kebutuhan fashion pria dan fashion wanita dengan menawarkan brand-brand terbaru dan terkemuka, baik lokal maupun internasional.
+
+Anda pastinya tahu bahwa penampilan belum sempurna tanpa kehadiran sepatu dan aksesoris. Mencari sepatu terbaru dengan kualitas terbaik tidaklah susah. Anda bisa menemukan dengan mudah sepatu wedges yang dapat dicocokan dengan dress ataupun gaun yang elegan, atau sepasang sneakers untuk melengkapi gaya kasual Anda. ZALORA Indonesia merupakan satu-satunya tempat belanja online fashion dimana Anda dapat menemukan semuanya, termasuk baju formal untuk ke pesta. Kami menawarkan koleksi pilihan fashion terlengkap dari kasual, busana muslim, formal, batik, jilbab, sports, dan sepatu. Tampilan Anda akan lebih lengkap dengan aksesoris dari Kami seperti tas, jam tangan digital & analog, kacamata, dan perhiasan. Dengan navigasi pencarian sederhana, daftar koleksi, dan harga yang menarik kami memberikan kemudahan untuk Anda berbelanja online untuk menemukan barang yang Anda butuhkan. Jadikanlah momen belanja fashion online Anda menjadi aktifitas yang menyenangkan dengan ZALORA Indonesia.
+
+Temukan ribuan brand lokal dan internasional untuk melengkapi kebutuhan fashion Anda. Apapun alasan Anda, nikmati gratis 30 hari pengembalian dengan mudah di seluruh Indonesia. ZALORA Indonesia memberikan jaminan produk 100% asli & berkualitas serta pengalaman belanja fashion online terbaik.
+</br>
+
+
+    </tr>
+    <p>
+      <p>
+        <p>
+          
+       
+    </p>
+
+
+    <span id="image"></span>
+    <tr>
+      <td height="5" width="1769" style="border-style: none; border-width: medium" colspan="7">
+      <img border="0" src="assets/img/womens-winter-deals-748x350-5.jpg" width="1300" height="450"></td>
+    </tr>
+    <tr>
+      <td height="19" width="1769" style="border-style: none; border-width: medium" colspan="7">&nbsp;
+      </td>
+    </tr>
+    <tr>
+      <td height="19" width="1769" style="border-style: none; border-width: medium" colspan="7">
+      <img border="0" src="assets/img/zine-mix-and-save-748x350.jpg" width="1300" height="450"></td>
+    </tr>
+    <tr>
+      <td height="19" width="1769" style="border-style: none; border-width: medium" colspan="7">&nbsp;
+      </td>
+    </tr>
+    <tr>
+      <td height="19" width="1769" style="border-style: none; border-width: medium" colspan="7">
+      <img border="0" src="assets/img/stance-women-boobies-748x350.jpg" width="1300" height="450"></td>
+    </tr>
+   
+
+    <tr>
+      <td height="19" width="1769" style="border-style: none; border-width: medium" colspan="7">
+      <img border="0" src="assets/img/nixon-brand-lifestyle-1400x445.jpg" width="1300" height="450"></td>
+    </tr>
+    <tr>
+      <td height="19" width="1769" style="border-style: none; border-width: medium" colspan="7">&nbsp;</td>
+    </tr>
+    <tr>
+      <td height="19" width="1769" style="border-style: none; border-width: medium" colspan="7">
+      <a href="add%20br.htm">
+      <img border="0" src="assets/img/ADIDAS.PNG" width="108" height="108"></a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+      <a href="DGK.htm">
+      <img border="0" src="assets/img/DGK.PNG" width="109" height="106"></a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+      <a href="DIAMOND%20BR.htm">
+      <img border="0" src="assets/img/DIAMOND2.PNG" width="108" height="109"></a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+      <a href="EMPYRE.htm">
+      <img border="0" src="assets/img/EM.PNG" width="107" height="109"></a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+      <a href="GK.htm">
+      <img border="0" src="assets/img/GLAMOUR%20KILL.PNG" width="107" height="107"></a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+      <a href="GR.htm">
+      <img border="0" src="assets/img/GRIZZLY.PNG" width="107" height="109"></a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+      <a href="GS.htm">
+      <img border="0" src="assets/img/GSHOCK.PNG" width="105" height="106"></a><a href="HUF.htm">
+        <img border="0" src="assets/img/HUF2.PNG" width="106" height="108"></a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+      <a href="MTT.htm">
+      <img border="0" src="assets/img/MTTB.PNG" width="109" height="109"></a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+      <a href="NEFF.htm">
+      <img border="0" src="assets/img/NEFF.PNG" width="109" height="107"></a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+      <a href="NIX.htm">
+      <img border="0" src="assets/img/NIXON2.PNG" width="107" height="108"></a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+      <a href="REB.htm">
+      <img border="0" src="assets/img/REBEL%208.PNG" width="107" height="107"></a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+      <a href="SB%20BR.htm">
+      <img border="0" src="assets/img/SB2.PNG" width="108" height="108"></a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+      <a href="SANTA.htm">
+      <img border="0" src="assets/img/SC.PNG" width="107" height="108"></a><a href="STUS.htm">
+        <img border="0" src="assets/img/STUSSY.PNG" width="107" height="107"></a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+      <a href="VAN.htm">
+      <img border="0" src="assets/img/VANS2.PNG" width="107" height="108"></a></td>
+    </tr>
+    <tr>
+      <td height="5" width="1769" style="border-style: none; border-width: medium" colspan="7">
+      <hr color="#000000" size="4">&nbsp;</td>
+    </tr>
+   
+
+
+
+
+
+  </table>
+  </center>
+</div>
+
+</body>
+
+</html>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    <div class="container">
+      <div class="row">
+       <!--  <div class="col-lg-8 col-md-10 mx-auto">
+          <div class="post-preview">
+            <a href="post.html">
+              <h2 class="post-title">
+                Man must explore, and this is exploration at its greatest
+              </h2>
+              <h3 class="post-subtitle">
+                Problems look mighty small from 150 miles up
+              </h3>
+            </a>
+            <p class="post-meta">Posted by
+              <a href="#">Start Bootstrap</a>
+              on September 24, 2018</p>
+          </div> -->
+          <!-- <hr>
+          <div class="post-preview">
+            <a href="post.html">
+              <h2 class="post-title">
+                I believe every human has a finite number of heartbeats. I don't intend to waste any of mine.
+              </h2>
+            </a>
+            <p class="post-meta">Posted by
+              <a href="#">Start Bootstrap</a>
+              on September 18, 2018</p>
+          </div>
+          <hr>
+          <div class="post-preview">
+            <a href="post.html">
+              <h2 class="post-title">
+                Science has not yet mastered prophecy
+              </h2>
+              <h3 class="post-subtitle">
+                We predict too much for the next year and yet far too little for the next ten.
+              </h3>
+            </a>
+            <p class="post-meta">Posted by
+              <a href="#">Start Bootstrap</a>
+              on August 24, 2018</p>
+          </div>
+          <hr>
+          <div class="post-preview">
+            <a href="post.html">
+              <h2 class="post-title">
+                Failure is not an option
+              </h2>
+              <h3 class="post-subtitle">
+                Many say exploration is part of our destiny, but it’s actually our duty to future generations.
+              </h3>
+            </a>
+            <p class="post-meta">Posted by
+              <a href="#">Start Bootstrap</a>
+              on July 8, 2018</p>
+          </div>
+          <hr> -->
+          <!-- Pager -->
+
+          <div class="clearfix">
+            <a class="btn btn-primary float-right" href="#">Older Posts &rarr;</a>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <hr>
+
+    <!-- Footer -->
+    <footer>
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-8 col-md-10 mx-auto">
+            <ul class="list-inline text-center">
+              <li class="list-inline-item">
+                <a href="#">
+                  <span class="fa-stack fa-lg">
+                    <i class="fa fa-circle fa-stack-2x"></i>
+                    <i class="fa fa-twitter fa-stack-1x fa-inverse"></i>
+                  </span>
+                </a>
+              </li>
+              <li class="list-inline-item">
+                <a href="#">
+                  <span class="fa-stack fa-lg">
+                    <i class="fa fa-circle fa-stack-2x"></i>
+                    <i class="fa fa-facebook fa-stack-1x fa-inverse"></i>
+                  </span>
+                </a>
+              </li>
+              <li class="list-inline-item">
+                <a href="#">
+                  <span class="fa-stack fa-lg">
+                    <i class="fa fa-circle fa-stack-2x"></i>
+                    <i class="fa fa-github fa-stack-1x fa-inverse"></i>
+                  </span>
+                </a>
+              </li>
+            </ul>
+            <p class="copyright text-muted">Copyright &copy; Your Website 2018</p>
+          </div>
+        </div>
+      </div>
+    </footer>
+
+    <!-- Bootstrap core JavaScript -->
+    <script src="assets/vendor/jquery/jquery.min.js"></script>
+    <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+    <!-- Custom scripts for this template -->
+    <script src="assets/js/clean-blog.min.js"></script>
+
+  </body>
+
+</html>
